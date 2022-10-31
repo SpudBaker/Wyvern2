c63fd004cbb142fd2cb4f41dda4f81a6501bf7db
Richard Baker <richard.baker@expensys.com>
2022-10-31 07:05:37 +0000

    rebuild project - deploy issue

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Wyvern2',
+  appName: 'testDeploy',
   webDir: 'www',
   bundledWebRuntime: false
 };
