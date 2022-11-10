447ea0f35d74bf8524400a6626d44c19ec00e707
Richard Baker <richard.baker@expensys.com>
2022-11-10 03:13:02 +0000

    build issue

--- src/app/stats/stats.page.ts
+++ src/app/stats/stats.page.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 import { GameService } from '../services/game';
-import { AuthService } from '../services/auth';
-import { User } from '@angular/fire/auth';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import * as Globals from '../../globals';
 
 @Component({
   selector: 'app-stats',
@@ -16,7 +12,7 @@ export class StatsPage {
 
   public usersArr$ : Observable<string[]>;
 
-  constructor(private authService: AuthService, private gameService: GameService, private router: Router) {
+  constructor(public gameService: GameService) {
     this.usersArr$ = this.gameService.getUsers().pipe(
       map(users => {
         return users;
