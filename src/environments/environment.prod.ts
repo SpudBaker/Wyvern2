307efa84429555d34fc54e43c02c3ba09450e93f
Richard Baker <richard.baker@expensys.com>
2022-12-23 20:01:31 +0000

    change firebase project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    apiKey: "AIzaSyB-m1LI54hq97q2GBmnj7erwaQfKcPUoEI",
-    authDomain: "wyverngame-1d3e9.firebaseapp.com",
-    projectId: "wyverngame-1d3e9",
-    storageBucket: "wyverngame-1d3e9.appspot.com",
-    messagingSenderId: "148120569958",
-    appId: "1:148120569958:web:0198f47efca7a4cef845a7",
-    measurementId: "G-96L7NN00BS"
+    apiKey: "AIzaSyBOZ3L5D2RBx_ZuoP5MJn6WEu_TAIgmP44",
+    authDomain: "pfriday-d8515.firebaseapp.com",
+    projectId: "pfriday-d8515",
+    storageBucket: "pfriday-d8515.appspot.com",
+    messagingSenderId: "32090802655",
+    appId: "1:32090802655:web:2702b1837a0460cd831c6a",
+    measurementId: "G-B8Q6ZDCR4T"
   },
   production: true
 };
